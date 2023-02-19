0fe260e6e68e5451c0682a19cb47afb86bd20346
Gary Willoughby <dev@nomad.uk.net>
2023-02-19 13:17:55 +0000

    Added new fading method for glyphs.

--- screen/saver/digital_rain/matrix/column/column.go
+++ screen/saver/digital_rain/matrix/column/column.go
@@ -60,6 +60,29 @@ func (c *Column) GlyphAfterIndex(index int) *glyph.Glyph {
 	return c.glyphs[index+1]
 }
 
+// FadeGlyphs fades highlighted glyphs.
+func (c *Column) FadeGlyphs() {
+	for i := 0; i < c.height; i++ {
+		// If there are any glyphs currently fading, remove the highlight.
+		if c.GlyphAtIndex(i).IsHighlightFading() {
+			c.GlyphAtIndex(i).RemoveHighlight()
+		}
+
+		if i < c.height-1 {
+			// Fade any highlighted glyph that has another added after it.
+			if c.GlyphAtIndex(i).IsNotEmpty() && c.GlyphAtIndex(i).IsHighlighted() && c.GlyphAfterIndex(i).IsNotEmpty() {
+				c.GlyphAtIndex(i).FadeHighlight()
+			}
+		} else {
+			// The bottom glyph is handled differently as you don't have to
+			// check the one after.
+			if c.GlyphAtIndex(i).IsNotEmpty() && c.GlyphAtIndex(i).IsHighlighted() {
+				c.GlyphAtIndex(i).FadeHighlight()
+			}
+		}
+	}
+}
+
 // AppendGlyphs add new glyphs to columns.
 func (c *Column) AppendGlyphs() {
 	for i := c.height - 1; i > -1; i-- {
@@ -71,27 +94,12 @@ func (c *Column) AppendGlyphs() {
 				}
 			}
 		} else {
-			// If the current glyph is fading, remove the highlight altogether.
-			if c.GlyphBeforeIndex(i).IsHighlightFading() {
-				c.GlyphBeforeIndex(i).RemoveHighlight()
-			}
-
-			// If we're at the bottom and the glyph is not empty and
-			// highlighted, fade the highlight.
-			if i == c.height-1 {
-				if c.GlyphAtIndex(i).IsNotEmpty() {
-					if c.GlyphAtIndex(i).IsHighlighted() {
-						c.GlyphAtIndex(i).FadeHighlight()
-					}
-				}
-			}
 			// Continue appending glyphs to the column.
 			if c.GlyphAtIndex(i).IsEmpty() && c.GlyphBeforeIndex(i).IsNotEmpty() {
 				if c.GlyphBeforeIndex(i).IsHighlighted() {
 					// If the glyph is highlighted and if chance favours it,
-					// skip adding a new one.
+					// add a new highlighted one.
 					if rand.Intn(c.highlightedGlyphStutterChance) != 0 {
-						c.GlyphBeforeIndex(i).FadeHighlight()
 						c.SetGlyphAtIndex(i, glyph.NewRandomHighlightedGlyph(c.GlyphBeforeIndex(i).IsSwitcherSpreader()))
 					}
 				} else {
@@ -138,6 +146,7 @@ func (c *Column) Iterate() {
 	c.AppendGlyphs()
 	c.SwitchGlyphs()
 	c.DeleteGlyphs()
+	c.FadeGlyphs()
 
 	c.iteration++
 }
